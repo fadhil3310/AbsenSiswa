65dcb784bf63a3b3480daaa835219b90def2b235
fadhil3310 <80736446+fadhil3310@users.noreply.github.com>
2023-05-21 21:05:25 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -15,13 +15,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('dashboard');
-});
-
-
-// Manjemen data
-Route::resource('datasiswa', 'App\Http\Controllers\SiswaController');
-Route::resource('dataguru', 'App\Http\Controllers\GuruController');
+    return view('index');
+})->middleware(['auth', 'verified'])->name('/');
 
 //Route::get('/dashboard', function () { return redirect('/'); });
 
